6f74080c4f4cbc78fb84e349bdbc811c4801aabf
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2022-04-23 20:04:12 +0800

    modified Appointment Component

--- frontend/src/appointments/Appointment.tsx
+++ frontend/src/appointments/Appointment.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "./Appointment.css";
 import {FaCalendarAlt} from "react-icons/fa"
+import { Container, Col, Row } from 'react-bootstrap';
 import AppointmentViewButtons from "./AppointmentViewButtons"
+import Header from "../common/Header";
+import Sidebars from "../common/Sidebars";
 import { Scheduler } from "@aldabil/react-scheduler";
 
 function Appointment() {
@@ -14,40 +17,55 @@ function Appointment() {
   
   return (
     <>
-      <div className="appointment">
-        <div className="appointment-container container">
-          <div className="appointment-label">
-            <FaCalendarAlt className="appointment-calendar-icon"/>
-            Appointments
-            <div className="appointment-view-btn-container">
-            <AppointmentViewButtons type={active} onClick={handleChange}/>
-            </div>
-          </div>
-          <div className={active === "calendar" ? "appointment-calendar-container" : "appointment-list-container"}>
-            {active === "calendar" ? (
-                <Scheduler
-                view="month"
-                events={[
-                  {
-                    event_id: 1,
-                    title: "Event 1",
-                    start: new Date("2021/5/2 09:30"),
-                    end: new Date("2021/5/2 10:30"),
-                  },
-                  {
-                    event_id: 2,
-                    title: "Event 2",
-                    start: new Date("2021/5/4 10:00"),
-                    end: new Date("2021/5/4 11:00"),
-                  },
-                ]}
-              />
-            ) : (
-              <div>Listttttttttt</div>
-            )}
-          </div>
-        </div>
-      </div>
+      <Container fluid>
+
+        <Row>
+          <Header />
+        </Row>
+
+        <Row>
+          <Col xs={2} className="sidebar-box p-0">
+            <Sidebars />
+          </Col>
+
+          <Col xs={10} className="appointment-box p-0 mt-2 border border-dark">
+            <Row>
+              <Col>
+                <FaCalendarAlt className="appointment-calendar-icon"/>
+                Appointments
+              </Col>
+              <Col>
+                <AppointmentViewButtons type={active} onClick={handleChange}/>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+              {active === "calendar" ? (
+                    <Scheduler
+                    view="month"
+                    events={[
+                      {
+                        event_id: 1,
+                        title: "Event 1",
+                        start: new Date("2021/5/2 09:30"),
+                        end: new Date("2021/5/2 10:30"),
+                      },
+                      {
+                        event_id: 2,
+                        title: "Event 2",
+                        start: new Date("2021/5/4 10:00"),
+                        end: new Date("2021/5/4 11:00"),
+                      },
+                    ]}
+                  />
+                ) : (
+                  <div>Listttttttttt</div>
+                )}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
