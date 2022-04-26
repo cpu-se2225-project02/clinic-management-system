4146f26146dda17e3754a69d30cb437978c9a9c4
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2022-04-26 15:42:05 +0800

    🤝 routed dashboard and clinic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-import Appointment from './appointments/Appointment';
+import Appointment from './appointment/Appointment';
+import Clinic from './clinic/Clinic';
+import Dashboard from './dashboard/Dashboard';
 import PatientList from './patient/PatientList';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <>
       <Router>
           <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/clinic" element={<Clinic />} />
             <Route path="/appointments" element={<Appointment/>}/>
             <Route path="/patients" element={<PatientList/>}/>
           </Routes>
