c4d6cadbce3dcf67dd65b35b49effbccdcaa5bd5
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-02 18:09:13 +0800

    Test new endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,6 @@ import bodyParser from 'body-parser';
 // import { PrismaClient } from '@prisma/client';
 
 const app = express();
-// const db = new PrismaClient();
 const PORT = 8000;
 
 app
@@ -13,6 +12,9 @@ app
   .use(bodyParser.json())
   .get('/', (req, res) => {
     res.send('Hello World');
+  })
+  .get('/def', (req, res) => {
+    res.send('Hola World!');
   })
   .listen(PORT, () => {
     console.log(`Server has started at http:://localhost:${PORT}`);
