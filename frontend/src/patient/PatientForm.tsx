08238c07a55e0cdf936c0f8d50332072f7f62b5c
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-05 23:55:13 +0800

    Add form states

--- frontend/src/patient/PatientForm.tsx
+++ frontend/src/patient/PatientForm.tsx
@@ -3,15 +3,29 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 
-import React from 'react';
+import React, { useState } from 'react';
 import './PatientForm.css';
 import { AiOutlineCloseSquare } from 'react-icons/ai';
+import { useMutation } from 'urql';
+import { AddPatientDocument } from '../queries.generated';
 
 interface Popup {
   postButton: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function PatientForm({ postButton }: Popup) {
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [middleInitial, setMiddileInitial] = useState('');
+  const [sex, setSex] = useState('');
+  const [suffix, setSuffix] = useState('');
+  const [age, setAge] = useState(0);
+  const [dob, setDob] = useState('');
+  const [address, setAddress] = useState('');
+  const [addPatient] = useMutation(AddPatientDocument);
+
+  const { data, error, fetching } = addPatient;
+
   return (
     <div className="popup">
       <div className="popup-inner">
@@ -23,13 +37,19 @@ export default function PatientForm({ postButton }: Popup) {
         </button>
 
         <label>Last name:</label>
-        <input className="form-control" type="text" placeholder="Last name" />
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Last name"
+          onChange={(e) => { setLastName(e.target.value); }}
+        />
 
         <label>First name:</label>
         <input
           className="form-control"
           type="text"
           placeholder="First name"
+          onChange={(e) => { setFirstName(e.target.value); }}
         />
 
         <label>Middle Initial:</label>
@@ -37,15 +57,22 @@ export default function PatientForm({ postButton }: Popup) {
           className="form-control"
           type="text"
           placeholder="Middle Initial"
+          onChange={(e) => { setMiddileInitial(e.target.value); }}
+
         />
 
         <label>Suffix:</label>
-        <input className="form-control" type="text" placeholder="Suffix" />
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Suffix"
+          onChange={(e) => { setSuffix(e.target.value); }}
+        />
 
         <label>Sex:</label>
-        <select className="form-control">
-          <option>Female</option>
-          <option>Male</option>
+        <select className="form-control" onChange={(e) => { setSex(e.target.value); }}>
+          <option value="Female">Female</option>
+          <option value="Male">Male</option>
         </select>
 
         <label>Date of Birth:</label>
@@ -53,10 +80,17 @@ export default function PatientForm({ postButton }: Popup) {
           className="form-control"
           type="date"
           placeholder="Date of Birth"
+          onChange={(e) => { setDob(e.target.value); }}
+
         />
 
         <label>Address:</label>
-        <input className="form-control" type="text" placeholder="Address" />
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Address"
+          onChange={(e) => { setAddress(e.target.value); }}
+        />
 
       </div>
     </div>
