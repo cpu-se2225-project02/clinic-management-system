a2fc00b2536fbb961e21e818db19e6ec8bc74c75
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-06 08:47:21 +0800

    Generated

--- frontend/src/generated/graphql.ts
+++ frontend/src/generated/graphql.ts
@@ -12,12 +12,38 @@ export interface Scalars {
   Float: number;
 }
 
+export interface Mutation {
+  readonly __typename?: 'Mutation';
+  readonly addPatient?: Maybe<Patient>;
+}
+
+
+export interface MutationAddPatientArgs {
+  newPatient: PatientInput;
+}
+
 export interface Patient {
   readonly __typename?: 'Patient';
   readonly address: Scalars['String'];
+  readonly age: Scalars['Int'];
+  readonly birthdate: Scalars['String'];
+  readonly f_name: Scalars['String'];
+  readonly id: Scalars['Int'];
+  readonly l_name: Scalars['String'];
+  readonly m_initial?: Maybe<Scalars['String']>;
+  readonly sex: Scalars['String'];
+  readonly suffix?: Maybe<Scalars['String']>;
+}
+
+export interface PatientInput {
+  readonly address: Scalars['String'];
+  readonly age: Scalars['Int'];
+  readonly birthdate: Scalars['String'];
   readonly f_name: Scalars['String'];
   readonly l_name: Scalars['String'];
-  readonly m_initial: Scalars['String'];
+  readonly m_initial?: InputMaybe<Scalars['String']>;
+  readonly sex: Scalars['String'];
+  readonly suffix?: InputMaybe<Scalars['String']>;
 }
 
 export interface Query {
