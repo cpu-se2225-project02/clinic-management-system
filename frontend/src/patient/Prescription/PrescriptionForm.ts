b575aeeba1af3176d87b03f89f17b035ddc49ed0
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2022-05-13 21:33:36 +0800

    added prescription schema

--- frontend/src/patient/Prescription/PrescriptionForm.ts
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import './PatientForm.css';
-import { AiOutlineCloseSquare } from 'react-icons/ai';
-import { useMutation } from 'urql';
-import { Modal, Spinner } from 'react-bootstrap';
-import { AddPrescription } from '../queries.generated';
-
-
-
