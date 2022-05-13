c1ef8c0ef385a3a47d464f34a95b4f79ca449dfa
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2022-05-13 20:18:45 +0800

    re

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-=======
-import React from "react";
 // import { render, screen } from '@testing-l ibrary/react';
 // import App from './App';
->>>>>>> 7a8919091f9f1161d1e048ecd5a013ef5a55aa84
 
 // test('renders learn react link', () => {
 //   render(<App />);
