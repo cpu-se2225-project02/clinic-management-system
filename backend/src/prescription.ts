a3df598b1e63ed08963fb530e49f89e22887aad4
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-14 11:01:32 +0800

    Add prescription form

---
+++ backend/src/prescription.ts
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+import {
+  inputObjectType,
+  mutationField, nonNull, objectType,
+} from 'nexus';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { Prescription as PrescriptionType } from 'nexus-prisma';
+
+const db = new PrismaClient();
+
+export const Prescription = objectType({
+  name: 'Prescription',
+  definition(t) {
+    t.field(PrescriptionType.pres_name);
+    t.field(PrescriptionType.pres_dos);
+  },
+});
+
+export const PrescriptionInput = inputObjectType({
+  name: 'PrescriptionInput',
+  definition(t) {
+    t.field(PrescriptionType.pres_name);
+    t.field(PrescriptionType.pres_dos);
+  },
+});
+
+export const AddPrescription = mutationField('addPrescription', {
+  type: Prescription,
+  args: {
+    newPrescription: nonNull(PrescriptionInput),
+  },
+  resolve(root, args: { newPrescription: Prisma.PrescriptionCreateInput }) {
+    return db.prescription.create({ data: args.newPrescription });
+  },
+});
---
+++ frontend/src/patient/prescription/UpdatePrescription.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+
+interface Popup {
+  editButton: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function UpdatePrescription({ editButton }: Popup) {
+  return (
+    <div className="popup">
+      <div className="popup-inner">
+        <button
+          onClick={() => editButton(false)}
+          className="btn close-btn float-end mt-0"
+        >
+          <AiOutlineCloseSquare size={25} />
+        </button>
+
+        <label>Prescription name:</label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Prescription name"
+        />
+
+        <label>Prescription dosage:</label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Prescription dosage"
+        />
+
+        <button
+          className="btn btn-primary mt-2 float-end"
+          type="submit"
+        >
+          Submit
+        </button>
+
+      </div>
+    </div>
+  );
+}
