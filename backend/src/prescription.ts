b5a90a16d94fd6ff1e7fa34749fc81f616867fc8
Kian Aira <90256501+marquisKian@users.noreply.github.com>
2022-05-16 00:11:36 +0800

    prescription update and delete

--- backend/src/prescription.ts
+++ backend/src/prescription.ts
@@ -1,7 +1,12 @@
 /* eslint-disable linebreak-style */
 import {
   inputObjectType,
-  mutationField, nonNull, objectType,
+  intArg,
+  list,
+  mutationField,
+  nonNull,
+  objectType,
+  queryField,
 } from 'nexus';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { Prescription as PrescriptionType } from 'nexus-prisma';
@@ -11,9 +16,28 @@ const db = new PrismaClient();
 export const Prescription = objectType({
   name: 'Prescription',
   definition(t) {
-    t.field(PrescriptionType.id);
     t.field(PrescriptionType.pres_name);
     t.field(PrescriptionType.pres_dos);
+  },
+});
+
+// read
+export const prescriptions = queryField('prescriptions', {
+  type: list(Prescription),
+  resolve() {
+    return db.prescription.findMany();
+  },
+});
+
+export const specificPrescription = queryField('specificPrescription', {
+  type: Prescription,
+  args: {
+    patientId: nonNull(intArg()),
+  },
+  resolve(root, args) {
+    return db.prescription.findUnique({
+      where: { id: args.prescriptionId },
+    });
   },
 });
 
@@ -32,5 +56,32 @@ export const AddPrescription = mutationField('addPrescription', {
   },
   resolve(root, args: { newPrescription: Prisma.PrescriptionCreateInput }) {
     return db.prescription.create({ data: args.newPrescription });
+  },
+});
+
+export const EditPrescription = mutationField('editPrescription', {
+  type: Prescription,
+  args: {
+    prescriptionId: nonNull(intArg()),
+    editedPrescription: nonNull(PrescriptionInput),
+  },
+  // eslint-disable-next-line max-len
+  resolve(root, args: { editedPrescription: Prisma.PrescriptionUpdateInput, prescriptionId: Prisma.PrescriptionWhereUniqueInput }) {
+    return db.prescription.update({
+      where: { id: args.prescriptionId as any },
+      data: args.editedPrescription,
+    });
+  },
+});
+
+export const DeletePrescription = mutationField('deletePrescription', {
+  type: Prescription,
+  args: {
+    prescriptionId: nonNull(intArg()),
+  },
+  resolve(root, args: { prescriptionId: Prisma.PrescriptionWhereUniqueInput }) {
+    return db.prescription.delete({
+      where: { id: args.prescriptionId as any },
+    });
   },
 });
