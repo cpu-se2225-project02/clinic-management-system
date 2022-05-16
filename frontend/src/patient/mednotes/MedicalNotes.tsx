9d4b0d9e805b584415e510a69e9a1ad6fcfa9140
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-16 22:52:46 +0800

    Add medical notes component

---
+++ frontend/src/patient/mednotes/MedicalNotes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GoDiffAdded } from 'react-icons/go';
+
+export default function MedicalNotes() {
+  return (
+    <>
+      <GoDiffAdded size={30} />
+      <div />
+    </>
+  );
+}
