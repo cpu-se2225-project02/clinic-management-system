ea8508b104a67d0871b159b44a5fcd3bf5ebe62e
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-17 10:33:13 +0800

    Add med notes form for adding new medical notes for a specific patient

---
+++ frontend/src/patient/mednotes/MedNotesForm.tsx
@@ -0,0 +1,84 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+import { useMutation } from 'urql';
+import { Spinner } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { AddAMedNoteDocument } from '../../queries.generated';
+import './MedNotesForm.css';
+
+interface PatientID {
+  pId: number | undefined
+  postButton: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function MedNotesForm({ pId, postButton }: PatientID) {
+  const [mnTitle, setMnTitle] = useState('');
+  const [mnDate, setMnDate] = useState('');
+  const [mnNotes, setMnNotes] = useState('');
+
+  const [AddMedoNoteResult, AddMedNote] = useMutation(AddAMedNoteDocument);
+  if (AddMedoNoteResult.fetching) {
+    return <Spinner animation="border" role="status" />;
+  }
+  if (AddMedoNoteResult.error) {
+    console.log(AddMedoNoteResult.error);
+    return <div>Insertion unsuccessful</div>;
+  }
+
+  const addingMedNote = () => {
+    AddMedNote({
+      newMedNote: {
+        title: mnTitle,
+        med_notes: mnNotes,
+        date_noted: mnDate,
+        patient_id: pId as number,
+      },
+    });
+  };
+  return (
+    <div className="popup">
+      <div className="popup-inner">
+        <button
+          onClick={() => postButton(false)}
+          className="btn close-btn float-end mt-0"
+        >
+          <AiOutlineCloseSquare size={25} />
+        </button>
+
+        <label>Title:</label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Enter title"
+          onChange={(e) => { setMnTitle(e.target.value); }}
+        />
+
+        <label>Date:</label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Enter date"
+          onChange={(e) => { setMnDate(e.target.value); }}
+
+        />
+
+        <label>Notes</label>
+        <textarea
+          className="form-control"
+          placeholder="Enter notes"
+          onChange={(e) => { setMnNotes(e.target.value); }}
+        />
+
+        <button
+          className="btn btn-primary mt-2 float-end"
+          onClick={addingMedNote}
+          type="submit"
+        >
+          Submit
+        </button>
+
+      </div>
+    </div>
+  );
+}
