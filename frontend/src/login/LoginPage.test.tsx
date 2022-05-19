1ee97b8fd9b1c6119cf31f4a90b25b104815ac8a
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-19 18:27:11 +0800

    Add test for login page frontend component

--- frontend/src/login/LoginPage.test.tsx
+++ frontend/src/login/LoginPage.test.tsx
@@ -1,10 +1,32 @@
-import React from "react"
-import {render} from "@testing-library/react"
-import LoginPage from "./LoginPage"
+/* eslint-disable no-undef */
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import LoginPage from './LoginPage';
 
-it("renders correctly", () => {
-    const {queryByPlaceholderText} = render(<LoginPage/>)
+it('renders correctly', () => {
+  const { queryByPlaceholderText } = render(<LoginPage />);
 
-    expect(queryByPlaceholderText("Enter email address")).toBeTruthy()
-    expect(queryByPlaceholderText("Enter password")).toBeTruthy()
-})
+  expect(queryByPlaceholderText('Enter email address')).toBeTruthy();
+  expect(queryByPlaceholderText('Enter password')).toBeTruthy();
+});
+
+describe('Input value', () => {
+  it('Email Updates on change', () => {
+    const { queryByPlaceholderText } = render(<LoginPage />);
+
+    const emailInput = queryByPlaceholderText('Enter email address') as HTMLInputElement;
+
+    fireEvent.change(emailInput, { target: { value: 'jennyrose@gmail.com' } });
+
+    expect(emailInput.value).toBe('jennyrose@gmail.com');
+  });
+
+  it('Password updates on change', () => {
+    const { queryByPlaceholderText } = render(<LoginPage />);
+
+    const passwordInput = queryByPlaceholderText('Enter password') as HTMLInputElement;
+
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    expect(passwordInput.value).toBe('password');
+  });
+});
