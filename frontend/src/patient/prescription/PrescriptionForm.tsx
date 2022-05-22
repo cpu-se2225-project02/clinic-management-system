5bbe5c5297e1fb7b4b3f5b7e4d54bd777db1eef5
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2022-05-23 02:17:25 +0800

    fixed prescription defects 🐛🐜🐞

--- frontend/src/patient/prescription/PrescriptionForm.tsx
+++ frontend/src/patient/prescription/PrescriptionForm.tsx
@@ -1,23 +1,26 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable radix */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import { useMutation } from 'urql';
 import { Spinner } from 'react-bootstrap';
-import { BiEdit } from 'react-icons/bi';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
 import { AddPrescriptionDocument } from '../../queries.generated';
 // import UpdatePrescription from './UpdatePrescription';
 
-interface PatientID {
+interface PrescFormProps {
   pID: number | undefined
+  popup: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function PrescriptionForm({ pID }: PatientID) {
-  // const [prescName, setPrescName] = useState('');
-  // const [dosage, setDosage] = useState(0);
-  const [editBtn, setEditBtn] = useState(false);
-  const [addPrescriptionResult] = useMutation(AddPrescriptionDocument);
+export default function PrescriptionForm(prescFormProps: PrescFormProps) {
+  const [prescName, setPrescName] = useState('');
+  const [dosage, setDosage] = useState(0);
+  // const [editBtn, setEditBtn] = useState(false);
+  const [addPresc, setAddPresc] = useMutation(AddPrescriptionDocument);
 
-  const { error, fetching } = addPrescriptionResult;
+  const { error, fetching } = addPresc;
 
   if (fetching) {
     return <Spinner animation="border" role="status" />;
@@ -27,57 +30,59 @@ export default function PrescriptionForm({ pID }: PatientID) {
     return <div>Insertion unsuccessful</div>;
   }
 
-  // const addingPrescription = () => {
-  //   addPrescription({
-  //     newPresc: {
-  //       pres_name: prescName,
-  //       pres_dos: dosage,
-  //     },
-  //   });
-  // };
+  const addPrescription = () => {
+    setAddPresc({
+      newPrescription: {
+        pres_name: prescName,
+        pres_dos: dosage,
+        patient_id: prescFormProps.pID as number,
+      },
+    });
+  };
+  const onSubmitBtnClicked = () => {
+    addPrescription();
+    prescFormProps.popup(false);
+  };
 
   return (
-    <div>
-      <div className="card border-dark mb-3">
-        <div className="h5">
-          <div className="col">Prescription</div>
-        </div>
+    <div className="popup">
+      <div className="popup-inner">
+        <h5>Add Payment</h5>
+        <button
+          onClick={() => prescFormProps.popup(false)}
+          className="btn close-btn float-end mt-0"
+        >
+          <AiOutlineCloseSquare size={25} />
+        </button>
 
-<<<<<<< HEAD
         <div className="col" style={{ textAlign: 'right' }}>
-          <div>{pID}</div>
-          <BiEdit size={30} onClick={() => setEditBtn(!editBtn)} />
-          {editBtn && <UpdatePrescription editButton={setEditBtn} />}
+          {/* <BiEdit size={30} onClick={() => setEditBtn(!editBtn)} /> */}
+          {/* {editBtn && <UpdatePrescription editButton={setEditBtn} />} */}
         </div>
         <div className="col">
-          <label>Prescriptions:</label>
-          <p>
-            PrescriptionName - Dosage
-          </p>
-          {/* <input
-=======
-      <div className="col" style={{ textAlign: 'right' }}>
-        <div>{pID}</div>
-        <BiEdit size={30} onClick={() => setEditBtn(!editBtn)} />
-        {/* {editBtn && <UpdatePrescription editButton={setEditBtn} />} */}
-      </div>
-      <div className="col">
-        <label>Prescriptions:</label>
-        <p>
-          PrescriptionName - Dosage
-        </p>
-        {/* <input
->>>>>>> badc6bd53dd9891feed4eef217b601efa349e2e8
-          type="text"
-          placeholder="Prescription"
-          onChange={(e) => setPrescName(e.target.value)}
-        /> */}
-          {/* <label>Dosage</label>
-        <input
-          type="number"
-          placeholder="Dosage"
-          onChange={(e) => setDosage(parseInt(e.target.value))}
-        /> */}
+          <div>
+            <input
+              type="text"
+              placeholder="Enter Prescription"
+              onChange={(e) => setPrescName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Enter Dosage"
+              onChange={(e) => setDosage(parseInt(e.target.value))}
+            />
+          </div>
+
+          <button
+            className="btn btn-primary mt-2 float-end"
+            onClick={onSubmitBtnClicked}
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
