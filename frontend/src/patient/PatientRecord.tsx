b63b0703899db3729f707a645c71f21244cf6e32
Ericka Guelos <erickajoyguelos@gmail.com>
2022-05-24 08:11:51 +0800

    Add test Id for testing

--- frontend/src/patient/PatientRecord.tsx
+++ frontend/src/patient/PatientRecord.tsx
@@ -33,7 +33,7 @@ export default function PatientRecord() {
   const { data } = allPatients;
 
   return (
-    <Container fluid className="PatientInfo">
+    <Container fluid test-dataid="PatientRecord" className="PatientInfo">
       <Row>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <Header />
