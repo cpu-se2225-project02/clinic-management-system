9cf420f44c32f0d10246244558a4d6d611702c1c
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-05-25 15:37:19 +0800

    Add total payments card

--- frontend/src/finance/FinancePage.tsx
+++ frontend/src/finance/FinancePage.tsx
@@ -1,36 +1,68 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable max-len */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import {
-  Col, Row, Container, Form, Dropdown, Button, Card,
+  Col, Row, Container, Form, Dropdown, Button, Card, Table,
 } from 'react-bootstrap';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { MdOutlinePayment, MdPayments } from 'react-icons/md';
 import { FaFileInvoice } from 'react-icons/fa';
+import { useQuery } from 'urql';
+import { GrMoney } from 'react-icons/gr';
 import Header from '../common/Header';
 import Sidebars from '../common/Sidebars';
 import AddPaymentForm from '../patient/account/AddPaymentForm';
 import Footer from '../common/Footer';
+import { AllPaymentsDocument } from '../queries.generated';
 
 export default function FinancePage() {
   const [AddPaymentBtn, setAddPaymentBtn] = useState(false);
   const [PayForm, setPayForm] = useState(false);
   const handleShowPayForm = () => setPayForm(true);
+  const [allPayments] = useQuery({
+    query: AllPaymentsDocument,
+  });
+  console.log(allPayments.data?.allPayments);
 
   return (
     <Container fluid>
       <Row>
         <Header />
       </Row>
+
       <Row>
         <Col xs={2} className="sidebar-box p-0">
           <Sidebars />
         </Col>
 
         <Col xs={10}>
-          <Row>
+          <Row className="mt-3">
+            <Col>
+              <Card style={{
+                width: '18rem',
+                textAlign: 'center',
+                boxShadow: '0px 0px 6px 5px rgba(209,209,209,1)',
+              }}
+              >
+                <Card.Body>
+                  <div>
+                    Php 00.00
+                  </div>
+                </Card.Body>
+                <Card.Footer>
+                  <GrMoney size={20} />
+                  {' '}
+                  Total Payments
+                </Card.Footer>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="mt-3">
             <Col xs={4} className="mt-2">
               <Card style={{
                 width: '18rem',
@@ -88,6 +120,7 @@ export default function FinancePage() {
               </Card>
             </Col>
           </Row>
+
         </Col>
       </Row>
       <Footer />
