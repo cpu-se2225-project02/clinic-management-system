5b33bca67ef962447c985c5e70ba894c57d814f7
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-05 17:34:39 +0800

    Update Referral.tsx

--- frontend/src/patient/referral/Referral.tsx
+++ frontend/src/patient/referral/Referral.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
-import { Spinner } from 'react-bootstrap';
+import { Spinner, Table } from 'react-bootstrap';
 import { BiEdit } from 'react-icons/bi';
 import { MdDeleteOutline } from 'react-icons/md';
 import { RiAddFill } from 'react-icons/ri';
@@ -76,59 +76,61 @@ function Referral({ pID }: PatientID) {
           popup={setAddReferral}
         />
       )}
-      <div className="col-sm-11">
+      <div>
         <button className="btn btn-outline-secondary" onClick={() => setAddReferral(true)}>
           <RiAddFill size={30} />
           Add Referral
         </button>
       </div>
       <div>
-        {data?.patientReferrals?.map((referral) => (
-          <>
-            <div className="btn-group editAndDelete" role="group">
-              <button type="button" className="editAndDltBtn" onClick={() => onEditBtnClicked(referral?.id as number)}>
-                <BiEdit size={30} />
-              </button>
-              <button type="button" className="editAndDltBtn" onClick={() => onDeleteBtnClicked(referral?.id as number)}>
-                <MdDeleteOutline size={30} />
-              </button>
-            </div>
-            {updateReferral && (
-              <UpdateReferralForm
-                pID={pID}
-                popup={setUpdatReferral}
-                refID={editBtnValue}
-                updateReferral={updateReferral}
-                updateReferralBtn={setUpdatReferral}
-              />
-            )}
-            {deleteConfirmation && (
-              <ConfirmDelete
-                onDeleteTrue={handleDeleteTrue}
-                onDeleteFalse={handleDeleteFalse}
-                deleteModal={deleteConfirmation}
-                deleteModalBtn={setDeleteConfirmation}
-              />
-            )}
-            <div className="row g-0 referredHospital">
-              <div className="col-sm-6 col-md-8">
-                <b>{referral?.hosp_name}</b>
-              </div>
-              <div className="col-6 col-md-4">
-                {' '}
-                Referred by:
-                {' '}
-                Dr.
-                {' '}
-                {referral?.doctor?.doc_name}
-              </div>
-            </div>
-            <hr />
-          </>
-        ))}
+        {/* <> */}
+        <Table className="table">
+          <tbody>
+            {data?.patientReferrals?.map((referral) => (
+              <tr>
+                <th scope="row">{referral?.hosp_name}</th>
+                <td>
+                  Referred by:
+                  {' '}
+                </td>
+                <td>
+                  Dr.
+                  {' '}
+                  {referral?.doctor?.doc_name}
+                </td>
+                <td>
+                  <div className="btn-group editAndDelete" role="group">
+                    <button type="button" className="editAndDltBtn" onClick={() => onEditBtnClicked(referral?.id as number)}>
+                      <BiEdit size={30} />
+                    </button>
+                    <button type="button" className="editAndDltBtn" onClick={() => onDeleteBtnClicked(referral?.id as number)}>
+                      <MdDeleteOutline size={30} />
+                    </button>
+                  </div>
+                  {updateReferral && (
+                    <UpdateReferralForm
+                      pID={pID}
+                      popup={setUpdatReferral}
+                      refID={editBtnValue}
+                      updateReferral={updateReferral}
+                      updateReferralBtn={setUpdatReferral}
+                    />
+                  )}
+                  {deleteConfirmation && (
+                    <ConfirmDelete
+                      onDeleteTrue={handleDeleteTrue}
+                      onDeleteFalse={handleDeleteFalse}
+                      deleteModal={deleteConfirmation}
+                      deleteModalBtn={setDeleteConfirmation}
+                    />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     </>
   );
 }
-
 export default Referral;
