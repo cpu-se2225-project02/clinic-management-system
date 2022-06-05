b9df2994b42479a23baf1ac7a1c39241286e6a2d
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-05 22:26:04 +0800

    Update medHistory.tsx

--- frontend/src/patient/medHistory/medHistory.tsx
+++ frontend/src/patient/medHistory/medHistory.tsx
@@ -6,7 +6,7 @@
 import React, { useState } from 'react';
 import { useMutation, useQuery } from 'urql';
 import {
-  Spinner, Table, Row, Col,
+  Spinner, Table,
 } from 'react-bootstrap';
 import { RiAddFill } from 'react-icons/ri';
 import { BiEdit } from 'react-icons/bi';
@@ -76,75 +76,68 @@ export default function MedicalHistory({ pID }: PatientID) {
   return (
     <>
       {addMedHistoryBtn && (
-      <MedHistoryForm
-        medHistorypopup={addMedHistoryBtn}
-        medHistoryBtn={setAddMedHistoryBtn}
-        popup={setAddMedHistoryBtn}
-        pID={pID as number}
-      />
+        <MedHistoryForm
+          medHistorypopup={addMedHistoryBtn}
+          medHistoryBtn={setAddMedHistoryBtn}
+          popup={setAddMedHistoryBtn}
+          pID={pID as number}
+        />
       )}
-      <div className="col-sm-11">
-        <button onClick={() => { setAddMedHistoryBtn(true); }} className="btn btn-outline-secondary">
-          <RiAddFill size={30} />
-          Add Medical History
-        </button>
-      </div>
 
-      {data?.patientMedHistory?.length !== 0
-        ? data?.patientMedHistory?.map((MedicalHistory) => (
-          <>
-            <div className="container">
-              <div className="row">
-                <div className="col">
+      <Table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Diagnosis</th>
+            <th scope="col">Treatment Plan</th>
+            <th scope="col">Description</th>
+            <th scope="col">
+              <button onClick={() => { setAddMedHistoryBtn(true); }} className="btn btn-outline-secondary">
+                <RiAddFill size={30} />
+                Add Medical History
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.patientMedHistory?.length !== 0
+            ? data?.patientMedHistory?.map((MedicalHistory) => (
+              <tr>
+                <td>{MedicalHistory?.diagnosis}</td>
+                <td>{MedicalHistory?.treatment_plan}</td>
+                <td>{MedicalHistory?.description}</td>
+                <td>
                   <div className="btn-group editAndDelete" role="group">
                     <button type="button" className="editAndDelete" onClick={() => onEditBtnClicked(MedicalHistory?.id as number)}>
                       <BiEdit size={30} />
                     </button>
-
                     <button type="button" className="editAndDelete" onClick={() => onDeleteBtnClicked(MedicalHistory?.id as number)}>
-
                       <MdDeleteOutline size={30} />
                     </button>
 
                     {updateMedHistoryBtn && (
-                    <UpdateMedHistoryForm
-                      popup={setUpdateMedHistoryBtn}
-                      pID={pID as number}
-                      medHistoryID={editBtnValue}
-                      updateMedHistory={updateMedHistoryBtn}
-                      updateMedHistoryBtn={setUpdateMedHistoryBtn}
-                    />
+                      <UpdateMedHistoryForm
+                        popup={setUpdateMedHistoryBtn}
+                        pID={pID as number}
+                        medHistoryID={editBtnValue}
+                        updateMedHistory={updateMedHistoryBtn}
+                        updateMedHistoryBtn={setUpdateMedHistoryBtn}
+                      />
                     )}
                     {deleteConfirmation && (
-                    <ConfirmDelete
-                      onDeleteTrue={handleDeleteTrue}
-                      onDeleteFalse={handleDeleteFalse}
-                      deleteModal={deleteConfirmation}
-                      deleteModalBtn={setDeleteConfirmation}
-                    />
+                      <ConfirmDelete
+                        onDeleteTrue={handleDeleteTrue}
+                        onDeleteFalse={handleDeleteFalse}
+                        deleteModal={deleteConfirmation}
+                        deleteModalBtn={setDeleteConfirmation}
+                      />
                     )}
                   </div>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div className="Medical History">
-                <div className="History">
-                  {MedicalHistory?.diagnosis}
-                </div>
-                Diagnosis:
-                {' '}
-                {MedicalHistory?.treatment_plan}
-                {' '}
-                {MedicalHistory?.description}
-                {' '}
-              </div>
-              <hr />
-            </div>
-          </>
-        ))
-        : <div>No Medical History given to patient.</div>}
+                </td>
+              </tr>
+            ))
+            : <div>No Medical History given to patient.</div>}
+        </tbody>
+      </Table>
     </>
   );
 }
