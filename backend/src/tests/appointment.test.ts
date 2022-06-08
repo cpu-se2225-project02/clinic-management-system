282153c36b5c48c20e58e10749b66c77afe259fa
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-08 22:50:22 +0800

    Update appointment.test.ts

--- backend/src/tests/appointment.test.ts
+++ backend/src/tests/appointment.test.ts
@@ -1,13 +1,10 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-undef */
-import { Appointment, Prisma } from '@prisma/client';
+import { Appointment } from '@prisma/client';
 import { MockContext, Context, createMockContext } from '../context';
-import { addAppointmen } from '../appointment';
+import { createAppointment, CreateAppointmentType } from '../appointment';
 
 let mockCtx: MockContext;
 let ctx: Context;
 
-// eslint-disable-next-line no-use-before-define
 beforeEach(() => {
   mockCtx = createMockContext();
   ctx = mockCtx as unknown as Context;
@@ -25,7 +22,7 @@ it('should test adding an appointment', async () => {
 
   mockCtx.prisma.appointment.create.mockResolvedValue(appointment);
 
-  await expect(addAppointmen(appointment, ctx)).resolves.toEqual({
+  await expect(createAppointment(appointment, ctx)).resolves.toEqual({
     id: 1,
     doc_id: 1,
     dt_end: 'Jan 1, 2022',
@@ -34,27 +31,12 @@ it('should test adding an appointment', async () => {
     patient_id: 1,
   });
 });
-<<<<<<< HEAD
-=======
-
-// async/await can be used.
-// it('works with async/await', async () => {
-//   expect.assertions(1);
-//   const data = await user.getUserName(4);
-//   expect(data).toEqual('Mark');
-// });
-
-// // async/await can also be used with `.resolves`.
-// it('works with async/await and resolves', async () => {
-//   expect.assertions(1);
-//   await expect(user.getUserName(5)).resolves.toEqual('Paul');
-// });
 
 function beforeEach(arg0: () => void) {
   throw new Error('Function not implemented.');
 }
 
-function expect(arg0: Prisma.Prisma__AppointmentClient<Appointment>) {
+
+function expect(arg0: any) {
   throw new Error('Function not implemented.');
 }
->>>>>>> main
