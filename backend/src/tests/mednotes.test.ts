126a8c4e0ff8e34249d911ab15bbaa621296c27e
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-06-11 13:10:07 +0800

    Add test for getting patient modes

--- backend/src/tests/mednotes.test.ts
+++ backend/src/tests/mednotes.test.ts
@@ -2,7 +2,7 @@
 import { MedicalNotes, Prisma } from '@prisma/client';
 import { MockContext, Context, createMockContext } from '../context';
 import {
-  createMedNote, getAMedNote,
+  createMedNote, getAMedNote, getPatientMedicalNotes,
 } from '../medNotes';
 
 let mockCtx: MockContext;
@@ -38,9 +38,7 @@ it('should test adding of a medical note', async () => {
 it('should test getting a specific medical note', async () => {
   mockCtx.db.medicalNotes.findUnique.mockResolvedValue(NoteA);
 
-  const patientId: Prisma.MedicalNotesWhereUniqueInput = {
-    id: 1,
-  };
+  const patientId: number = 1;
 
   await expect(getAMedNote(patientId, ctx)).resolves.toEqual({
     id: 1,
@@ -51,4 +49,19 @@ it('should test getting a specific medical note', async () => {
     doc_id: 1,
 
   });
+});
+
+it('should test getting mednotes of a patient', async () => {
+  const patientId: number = 1;
+
+  mockCtx.db.medicalNotes.findMany.mockResolvedValue([NoteA]);
+
+  await expect(getPatientMedicalNotes(patientId, ctx)).resolves.toEqual([{
+    id: 1,
+    date_noted: '2012-12-22',
+    med_notes: 'This man needs some milk',
+    title: 'Tale of Milk',
+    patient_id: 1,
+    doc_id: 1,
+  }]);
 });
