09f552ea35a385bf74e436b2694513f5a542f19f
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-19 00:56:54 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-// import { createClient, Provider as UrqlProvider } from 'urql';
+import { createClient, Provider as UrqlProvider } from 'urql';
 import Appointment from './appointment/Appointment';
 import Clinic from './clinic/Clinic';
 import Dashboard from './dashboard/Dashboard';
@@ -15,27 +15,28 @@ import LoginPage from './authentication/LoginPage';
 import PatientRecord from './patient/PatientRecord';
 import SingUpPage from './authentication/SignupPage';
 
-// const client = createClient({
-//   url: 'http://localhost:8001/graphql',
-// });
+const client = createClient({
+  url: 'http://localhost:8001/graphql',
+});
 
 function App() {
   return (
-    // <UrqlProvider value={client}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/clinic" element={<Clinic />} />
-        <Route path="/appointments" element={<Appointment />} />
-        <Route path="/patients/*" element={<PatientList />} />
-        <Route path="/finance" element={<FinancePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<SingUpPage />} />
-        <Route path="/patient_record/:id" element={<PatientRecord />} />
-        <Route path="/invoice/:id" element={<InvoiceForm />} />
-      </Routes>
-    </Router>
-    // </UrqlProvider>
+    <UrqlProvider value={client}>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/clinic" element={<Clinic />} />
+          <Route path="/appointments" element={<Appointment />} />
+          <Route path="/patients/*" element={<PatientList />} />
+          <Route path="/finance" element={<FinancePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<SingUpPage />} />
+          <Route path="/patient_record/:id" element={<PatientRecord />} />
+          <Route path="/invoice/:id" element={<InvoiceForm />} />
+        </Routes>
+      </Router>
+    </UrqlProvider>
   );
 }
 
