f1ab8c49bb2609a49a0bfb472a7f32cdee85c1d7
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-19 02:57:45 +0800

    -

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { env } from 'process';
 // import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
 app
-  // .use(cors())
-  // .use(bodyParser.json())
+  .use(cors())
+  .use(bodyParser.json())
   .listen(PORT, () => {
     console.log(`Server has started at http:://localhost:${PORT}`);
   });
