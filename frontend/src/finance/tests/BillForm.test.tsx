7030a8f7a2d890ae25c187aa997dd4b8b16fe912
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-20 03:50:33 +0800

    Update BillForm.test.tsx

--- frontend/src/finance/tests/BillForm.test.tsx
+++ frontend/src/finance/tests/BillForm.test.tsx
@@ -7,19 +7,19 @@ import BillForm from '../BillForm';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 
+
 describe('Bill Form', () => {
   it('renders correctly', async () => {
-     render(
-      <Router>
-        <Routes>
-          <Route path="*" element={<BillForm addPaymentBtn={function (value: SetStateAction<boolean>): void {
-                    throw new Error('Function not implemented.');
-                } } payForm={false} />} />
-        </Routes>
-      </Router>,
-    );
+     render(<BillForm />);
   });
 });
+
+// it('Renders correctly', () => {
+//   const { queryByTestId } = render(<BillForm/>);
+
+//   expect(queryByTestId('billForm')).toBeTruthy();
+// });
+
 // describe('Input value', () => {
 //     it('updates amount cost', () => {
 //       const { queryByPlaceholderText } = render(<BillForm />);
