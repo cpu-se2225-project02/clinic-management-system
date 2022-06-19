20e944a32d52a90fd1ce07027023328235261e8d
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-20 03:35:51 +0800

    made test folder and moved common folder test files

--- frontend/src/common/tests/Header.test.tsx
+++ frontend/src/common/tests/Header.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render } from '@testing-library/react';
-import Header from './Header';
+import Header from '../Header';
 
 it('renders correctly', () => {
   const { queryByTestId } = render(<Header />);
