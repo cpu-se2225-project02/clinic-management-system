f70e64108e6b13328ba7ebf6c9e017395bf587f5
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-20 03:22:36 +0800

    Rendered Component in Appointment Folder, Updated PatientAccPopup

--- frontend/src/appointment/tests/Appointment.test.tsx
+++ frontend/src/appointment/tests/Appointment.test.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { fireEvent, getByRole, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PatientAccountPopup from '../PatientAccountPopup';
+import Appointment from '../Appointment';
 
-describe('Patient Account Popup', () => {
+
+describe('Appointment Page', () => {
     it('renders correctly', async () => {
        render(
         <Router>
           <Routes>
-            <Route path="*" element={<PatientAccountPopup/>}/>
+            <Route path="*" element={<Appointment/>}/>
           </Routes>
         </Router>,
       );
     });
   });
+
+
+
--- frontend/src/appointment/tests/AppointmentViewButtons.test.tsx
+++ frontend/src/appointment/tests/AppointmentViewButtons.test.tsx
@@ -1,16 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { fireEvent, getByRole, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PatientAccountPopup from '../PatientAccountPopup';
+import AppointmentViewButtons from '../AppointmentViewButtons';
 
-describe('Patient Account Popup', () => {
+
+describe('Appointment View Buttons', () => {
     it('renders correctly', async () => {
        render(
         <Router>
           <Routes>
-            <Route path="*" element={<PatientAccountPopup/>}/>
+            <Route path="*" element={<AppointmentViewButtons type={''} onClick={undefined}/>}/>
           </Routes>
         </Router>,
       );
     });
   });
+
+
+
