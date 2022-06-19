2f3bd014cf75878672c9e996b0e15506512925b6
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-06-19 20:45:40 +0800

    Add server config from mock server

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,5 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { server } from './mocks/server';
+
+beforeAll(() => {
+  server.listen({
+    onUnhandledRequest: 'error',
+  });
+});
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
