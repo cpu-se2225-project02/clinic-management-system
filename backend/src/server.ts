083122abec301def5ae68416a03303775fdd0b31
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2022-06-19 18:52:22 +0800

    reverted all files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ import { env } from 'process';
 // import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app
   .use(cors())
