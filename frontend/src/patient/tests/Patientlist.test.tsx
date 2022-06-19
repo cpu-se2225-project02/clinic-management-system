2e77d6a8f81e42d165992be02e5a486d3add9753
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-06-20 02:31:50 +0800

    Separate mock client for reuse

--- frontend/src/patient/tests/Patientlist.test.tsx
+++ frontend/src/patient/tests/Patientlist.test.tsx
@@ -4,17 +4,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'urql';
-import { never } from 'wonka';
 import PatientList from '../PatientList';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import 'isomorphic-unfetch';
-
-const mockClient = {
-  executeQuery: jest.fn(() => never),
-  executeMutation: jest.fn(() => never),
-  executeSubscription: jest.fn(() => never),
-};
+import { mockClient } from '../../mocks/MockClient';
 
 describe('All Patients', () => {
   it('triggers the query', async () => {
